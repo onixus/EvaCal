1d75ba131f8c6fc744c6b05701525ce51ff0b1f1
onixus <onixus@live.ru>
2026-08-09 14:55:30 +0300

    test(gost34): export project context normalizer

--- lib/gost34/context/index.ts
+++ lib/gost34/context/index.ts
@@ -1,3 +1,4 @@
 export * from './types';
 export { buildProjectContext, hasBlockingGaps } from './builder';
+export { normalizeProjectContextForGeneration } from './normalize';
 export type { ProjectContextInput } from './builder';
